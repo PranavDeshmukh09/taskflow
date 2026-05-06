ff229162ebf03419f6adc02df3a72ae009627c92
PranavDeshmukh09 <deshmukhpranav100@gmail.com>
2026-05-06 20:53:26 +0530

    Reverting all Files

--- taskflow-web/src/features/dashboard/dashboardApi.js
+++
@@ -1,8 +0,0 @@
-// src/features/dashboard/dashboardApi.js
-
-import apiClient from '../../shared/lib/axios';
-
-export const dashboardApi = {
-  getStats: () => apiClient.get('/dashboard/stats'),
-  getTrend: () => apiClient.get('/dashboard/trend'),
-};
