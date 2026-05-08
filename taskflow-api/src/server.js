0a0f565611aa97e4ad429598893653daca34fa8a
PranavDeshmukh09 <deshmukhpranav100@gmail.com>
2026-05-08 21:42:23 +0530

    Commit Server.js update 0.2(updating Code)

--- taskflow-api/src/server.js
+++ taskflow-api/src/server.js
@@ -2,6 +2,15 @@ const app = require('./app');
 const db = require('./models');
 //const startReminderCron = require('./jobs/reminderCronJob');
 const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+try {
+  
+
+} 
+catch (error) {
 
 
+
+}
+  };
 startServer();
