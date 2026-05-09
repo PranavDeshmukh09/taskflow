37cd9f4506e9c32fdf7e8487e36b59158df81fc6
PranavDeshmukh09 <deshmukhpranav100@gmail.com>
2026-05-09 22:42:48 +0530

    Commit 0.6 app.js

--- taskflow-api/src/app.js
+++ taskflow-api/src/app.js
@@ -18,3 +18,13 @@ const activityRouter = require('./routes/activity.router');
 const app = express();
 // Security middleware
 app.use(helmet());
+app.use(cors());
+app.use(compression());
+
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API routes
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/projects', projectRouter);
