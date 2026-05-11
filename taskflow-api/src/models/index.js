2fb09a26d82704cc903ccaf3ccf07306182038df
PranavDeshmukh09 <deshmukhpranav100@gmail.com>
2026-05-11 22:25:16 +0530

    Commit 0.14 (index.js)

--- taskflow-api/src/models/index.js
+++ taskflow-api/src/models/index.js
@@ -34,3 +34,31 @@ db.Task = require('./Task.model')(sequelize, Sequelize);
 db.TaskTag = require('./TaskTag.model')(sequelize, Sequelize);
 db.ReminderLog = require('./ReminderLog.model')(sequelize, Sequelize);
 db.ActivityLog = require('./ActivityLog.model')(sequelize, Sequelize);
+// ========== User Associations ==========
+db.User.hasMany(db.PasswordResetToken, { foreignKey: 'userId', as: 'resetTokens' });
+db.PasswordResetToken.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+db.User.hasMany(db.Project, { foreignKey: 'userId', as: 'projects' });
+db.Project.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+db.User.hasMany(db.Tag, { foreignKey: 'userId', as: 'tags' });
+db.Tag.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+db.User.hasMany(db.Task, { foreignKey: 'userId', as: 'tasks' });
+db.Task.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+// ========== Task Associations ==========
+db.Task.belongsTo(db.Project, { foreignKey: 'projectId', as: 'project' });
+db.Project.hasMany(db.Task, { foreignKey: 'projectId', as: 'tasks' });
+
+// Task <-> Tag (many-to-many through TaskTag)
+db.Task.belongsToMany(db.Tag, { through: db.TaskTag, foreignKey: 'taskId', as: 'tags' });
+db.Tag.belongsToMany(db.Task, { through: db.TaskTag, foreignKey: 'tagId', as: 'tasks' });
+// ReminderLog associations
+db.Task.hasMany(db.ReminderLog, { foreignKey: 'taskId', as: 'reminderLogs' });
+db.ReminderLog.belongsTo(db.Task, { foreignKey: 'taskId', as: 'task' });
+//Activitylog
+db.User.hasMany(db.ActivityLog, { foreignKey: 'userId', as: 'activityLogs' });
+db.ActivityLog.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+module.exports = db;
