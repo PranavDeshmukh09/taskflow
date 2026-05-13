850de5460961b1ac7b974d91c7d46ad8d379b957
PranavDeshmukh09 <deshmukhpranav100@gmail.com>
2026-05-14 05:20:10 +0530

    Commit 0.18 (ActivityLog.model.js)

---
+++ taskflow-api/src/models/ActivityLog.model.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const ActivityLog = sequelize.define('ActivityLog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'user_id'
+    },
+    
+    errorMessage: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      field: 'error_message'
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      field: 'created_at'
+    }
+  }, {
+    tableName: 'activity_logs',
+    timestamps: false,
+    underscored: true,
+    indexes: [
+      { fields: ['user_id'] },
+      { fields: ['action'] },
+      { fields: ['entity_type', 'entity_id'] },
+      { fields: ['created_at'] }
+    ]
+  });
+
+  return ActivityLog;
+};
